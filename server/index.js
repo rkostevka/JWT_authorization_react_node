1174770864a379be7541af618bb0aa68f7c81615
ruslana <ruslana.voznyuk@gmail.com>
2021-11-01 18:11:19 +0200

    schema of models

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
 	try {
 		await mongoose.connect(process.env.DB_URL, {
