b75d009f5a26c824c84702d0a31386374188ffa9
ruslana <ruslana.voznyuk@gmail.com>
2021-11-01 17:47:38 +0200

    connect to Mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
+const mongoose = require('mongoose');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 const start = async () => {
 	try {
+		await mongoose.connect(process.env.DB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
 		app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 	} catch (e) {
 		console.log(e);
