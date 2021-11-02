f70efe9e71686c4f25ae5308dbdcba4adacf23e5
ruslana <ruslana.voznyuk@gmail.com>
2021-11-02 12:17:34 +0200

    server: generate jwt access, refresh tokens

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -2,6 +2,8 @@ const userModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
 
 class UserService {
 	async registration (email, password) {
@@ -13,7 +15,17 @@ class UserService {
 		const hashPassword = await bcrypt.hash(password, 3);
 		const activationLink = uuid.v4();
 		const user = await userModel.create({email, password: hashPassword, activationLink});
+		//send activayionLink
 		await mailService.sendActivationMail(email, activationLink);
+		//generate tokens
+		const userDto = new UserDto(user); //id, email, isActivated
+		const tokens = tokenService.generateTokens({...userDto});
+		await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+		return {
+			...tokens,
+			user: userDto
+		}
 	}
 }
 
