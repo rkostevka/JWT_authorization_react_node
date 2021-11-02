5410b258e9931d6d78b537bd126efdcfbc558b4b
ruslana <ruslana.voznyuk@gmail.com>
2021-11-02 10:34:37 +0200

    server UserController

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,13 @@
 const Router = require('express').Router;
+const userController = require('../controllers/user-controller');
 
 const router = new Router();
 
-router.post('./registration');
-router.post('./login');
-router.post('./logout');
-router.post('./activate/:link');
-router.post('./refresh');
-router.post('./users');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
 
 module.exports = router;
