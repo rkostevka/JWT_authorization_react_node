cdf4a623489b7d9ab2b12e881116c98ffa6afdc4
ruslana <ruslana.voznyuk@gmail.com>
2021-11-03 13:08:25 +0200

    Server:validation body req(email, pass)

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,14 @@
 const userService = require('../service/user-service');
+const {validationResult} = require('express-validator');
+const ApiError = require('../exeptions/api-error');
 
 class UserController {
 	async registration(req, res, next) {
 		try {
+			const errors = validationResult(req);
+			if(!errors.isEmpty()){
+				return(next(ApiError.BadRequest('Error of validation', errors.array())))
+			};
 			const {email, password} = req.body;
 			const userData = await userService.registration(email, password);
 			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,14 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user-controller');
+const {body} = require('express-validator');
 
 const router = new Router();
 
-router.post('/registration', userController.registration);
+router.post(
+	'/registration',
+	body('email').isEmail(),
+	body('password').isLength({min: 5, max: 12}),
+	userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
