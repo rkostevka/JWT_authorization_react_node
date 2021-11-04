caa33e88e37305cfdc009e49c1816ead351e0750
ruslana <ruslana.voznyuk@gmail.com>
2021-11-04 10:50:57 +0200

    Server: middleware refresh token

---
+++ server/middlewares/auth-middleware.js
@@ -0,0 +1,23 @@
+const ApiError = require('../exeptions/api-error');
+const tokenService = require('../service/token-service');
+
+module.exports = function(req, res, next) {
+	try {
+		const authorizationHeader = req.headers.authorization;
+		if(!authorizationHeader) {
+			return next(ApiError.UnauthorizedError());
+		}
+		const accesToken = authorizationHeader.split(' ')[1];
+		if(!accesToken) {
+			return next(ApiError.UnauthorizedError());
+		}
+		const userData = tokenService.validateAccesToken(accesToken);
+		if(!userData) {
+			return next(ApiError.UnauthorizedError());
+		}
+		req.user = userData;
+		next();
+	} catch(e) {
+		return next(ApiError.UnauthorizedError());
+	}
+}
--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user-controller');
 const {body} = require('express-validator');
+const authMiddleware = require('../middlewares/auth-middleware');
 
 const router = new Router();
 
@@ -13,6 +14,6 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
+router.get("/users", authMiddleware, userController.getUsers);
 
 module.exports = router;
